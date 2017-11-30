5c01b8e5333bef6ad7fff31020e2b79fad683580
banut <teobanu93@gmail.com>
2017-11-30 12:25:13 +0200

    routing register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { PizzaListComponent } from './pizza/pizza-list/pizza-list.component';
-import { PizzaDetailsComponent } from './pizza/pizza-details/pizza-details.component';
-import { LoginComponent } from './login/login/login.component';
-import { RegisterComponent } from './login/register/register.component';
-import { AccountComponent } from './login/account/account.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {AppComponent} from './app.component';
+import {PizzaListComponent} from './pizza/pizza-list/pizza-list.component';
+import {PizzaDetailsComponent} from './pizza/pizza-details/pizza-details.component';
+import {LoginComponent} from './login/login/login.component';
+import {RegisterComponent} from './login/register/register.component';
+import {AccountComponent} from './login/account/account.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-let registerState = {name: 'register', url: '/register', component: RegisterComponent};
-let loginState = {name: 'login', url: '/login', component: LoginComponent};
+const appRoutes: Routes = [
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+    {path: '', component: PizzaListComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    PizzaListComponent,
-    PizzaDetailsComponent,
-    LoginComponent,
-    RegisterComponent,
-    AccountComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PizzaListComponent,
+        PizzaDetailsComponent,
+        LoginComponent,
+        RegisterComponent,
+        AccountComponent,
+        NavbarComponent,
+        PageNotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes, {enableTracing: true})
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {User} from '../user';
 import {UserService} from '../user.service';
 
 @Component({
-  selector: 'register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
-  providers: [UserService]
+    selector: 'register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
+    providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
-	user: User = new User();
+    user: User = new User();
 
-	constructor(private userService: UserService) { }
+    constructor(
+        private userService: UserService,
+        private router: Router) {
+    }
 
-	ngOnInit() {
-	}
+    ngOnInit() {
+    }
 
-	createUser(user: User) {
-	    this.userService.createUser(user).then((newUser: User) => {
-	        console.log("user created");
-	    });
-	}
+    createUser(user: User) {
+        this.userService.createUser(user).then((newUser: User) => {
+            this.router.navigate(['/login']);
+        });
+    }
 
 }
